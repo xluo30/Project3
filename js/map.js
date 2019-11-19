fd4ca38319d074dbebb409eb726f9a14778d6720
xluo30 <54563200+xluo30@users.noreply.github.com>
2019-11-19 00:18:53 -0600

    .

--- js/map.js
+++ js/map.js
@@ -4,21 +4,19 @@ function initMap() {
     center: {lat: 41.8556217, lng: -87.6358867},
     zoom: 12
   });
-  var image = "image/IIT.png";
+
   var beachMarker = new google.maps.Marker({
     position: {lat: 41.8455552, lng: -87.6283674},
     map: map,
     icon: image
   });
 
-  var image = "image/Lake.png";
   var beachMarker = new google.maps.Marker({
     position: {lat: 41.8443403, lng: -87.5795727},
     map: map,
     icon: image
   });
 
-  var image = "image/Downtown.png";
   var beachMarker = new google.maps.Marker({
     position: {lat: 41.8880741, lng: -87.6357704},
     map: map,
