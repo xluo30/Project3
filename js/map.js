f0b1f130e99b547df6c8eba60020dcecefcbab43
xluo30 <54563200+xluo30@users.noreply.github.com>
2019-11-26 23:18:13 -0600

    final

--- js/map.js
+++ js/map.js
@@ -1,27 +1,15 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 41.8556217, lng: -87.6358867},
-    zoom: 12
-  });
-
-  var beachMarker = new google.maps.Marker({
-    position: {lat: 41.8455552, lng: -87.6283674},
-    map: map,
-    icon: image
-  });
-
-  var beachMarker = new google.maps.Marker({
-    position: {lat: 41.8443403, lng: -87.5795727},
-    map: map,
-    icon: image
-  });
-
-  var beachMarker = new google.maps.Marker({
-    position: {lat: 41.8880741, lng: -87.6357704},
-    map: map,
-    icon: image
-  });
-}
-
-window.addEventListener('onload',initMap);
+     function initMap() {
+        var chongqing = {lat:29.4316, lng:  106.9123};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 5,
+          center: chongqing ,
+        });
+        var marker = new google.maps.Marker({
+          position: chongqing,
+          map: map
+		
+        });
+      }
+  
+	  window.addEventListener('load', chongqing);
